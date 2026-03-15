5a772dcfb1084a978264bab8bac939295f68081e
Lazar Hristov <lhristov@gmail.com>
2026-03-15 12:50:55 +0200

    fix: ensure rootfs is rw by adding 'rw' to bootargs

--- boot/boot.cmd
+++ boot/boot.cmd
@@ -1,7 +1,7 @@
 # Always boot from SD card (mmc 0) - ignore boot device detection
 setenv mmc_rootdev mmcblk0p2
 
-setenv bootargs console=ttyS0,115200 console=tty1 root=/dev/${mmc_rootdev} rootwait panic=10 ${extra}
+setenv bootargs console=ttyS0,115200 root=/dev/${mmc_rootdev} rootwait rw panic=10 ${extra}
 load mmc 0:1 ${fdt_addr_r} ${fdtfile} || load mmc 0:1 ${fdt_addr_r} boot/allwinner/${fdtfile}
 load mmc 0:1 ${kernel_addr_r} zImage || load mmc 0:1 ${kernel_addr_r} boot/zImage
 bootz ${kernel_addr_r} - ${fdt_addr_r}
